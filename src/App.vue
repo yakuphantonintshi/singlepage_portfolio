eb52f3667698841b3b23ba85c61b9aead3ad087d
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-26 11:44:11 +0200

    Footer

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <main class="container-fluid">
   <router-view/>
 </main>
+<Footer/>
 </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 }
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer class="container-fluid">
+    <div class="row">
+        <p class="lead">Yakupha's portfolio &copy;</p>
+        <span class="shadow">
+            {{ cYear }}
+        </span>
+    </div>
+
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+    data(){
+        return{
+            cYear: new Date().getUTCFullYear()
+        }
+    }, 
+
+}
+</script>
+
+<style>
+
+</style>
